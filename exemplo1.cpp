8225f4bff1527723f17a650e9dec88c5910e672e
Henrique Hänsch Stepic <38055978+HenriqueHanschStepic@users.noreply.github.com>
2018-05-22 18:54:48 -0300

    Create exemplo1.cpp

---
+++ exemplo1.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main () {
+	char operacao;
+	int resultado,
+		primeiro,
+		segundo;
+	printf("Qual operacao matematica: ");
+	scanf("%c", &operacao);
+	printf("Qual o primeiro numero: ");
+	scanf("%d", &primeiro);
+	printf("Qual o segundo numero: ");
+	scanf("%d", &segundo);
+	switch(operacao) {
+		case '+':
+			resultado = primeiro + segundo;
+			break;
+		case '-':
+			resultado = primeiro - segundo;
+			break;
+		case '*':
+			resultado = primeiro * segundo;
+			break;
+		case '/':
+		case ':':
+			resultado = primeiro / segundo;
+			break;
+		default:
+			printf("Valor invalido\n");
+			resultado = 0;
+			break;
+	}
+	printf("O resultado e %d", resultado);
+	return 0;
+}
